9b88b82dcc80c830ed45a089634e43c4d31dfb58
subh <subhranshu497@gmail.com>
2026-04-14 18:14:57 -0700

    04152026

---
+++ com/leetcode/april/ShortestDistancetoTargetStringinaCircularArray.java
@@ -0,0 +1,31 @@
+package com.com.leetcode.april;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShortestDistancetoTargetStringinaCircularArray {
+    public static void main(String[] args) {
+        String[] words = {"hello","i","am","leetcode","hello"};
+        String target = "hello";
+        int startIndex = 1;
+        int result = closetTarget(words, target, startIndex);
+        System.out.println(result); // Output the result
+    }
+
+    private static int closetTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int minDist = Integer.MAX_VALUE;
+        Map<String, List<Integer>> indexMap = new HashMap<>();
+        for(int i=0;i<n;i++)
+            indexMap.computeIfAbsent(words[i], k->new ArrayList<>()).add(i);
+        if(!indexMap.containsKey(target)) return -1;
+        List<Integer> targetIndices = indexMap.get(target);
+        for(int index : targetIndices){
+            int dist = Math.abs(index - startIndex);
+            minDist = Math.min(minDist, Math.min(dist, n - dist));
+        }
+        return minDist;
+    }
+}
