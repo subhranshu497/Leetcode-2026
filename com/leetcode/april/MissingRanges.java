28220623a34af930860262477caefb2dc05868fd
subh <subhranshu497@gmail.com>
2026-04-16 21:28:09 -0700

    04182026

--- com/leetcode/april/MissingRanges.java
+++ com/leetcode/april/MissingRanges.java
@@ -1,6 +1,7 @@
 package com.com.leetcode.april;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MissingRanges {
@@ -15,20 +16,17 @@ public class MissingRanges {
     private static List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
         int n = nums.length;
         List<List<Integer>> missingRanges = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            int prev = nums[i];
-            if(lower==prev){
-                continue;
-            }else if(i>0 && nums[i-1]+1==nums[i]) continue;
-            else if(i < n){
-                int l_bound = nums[i-1]+1;
-                int u_bound = nums[i]-1;
-                missingRanges.add(List.of(l_bound, u_bound));
-            }else{
-                int l_bound = nums[i-1]+1;
-                int u_bound = upper;
-                missingRanges.add(List.of(l_bound, u_bound));
-            }
+        if (n == 0) {
+            missingRanges.add(Arrays.asList(lower, upper));
+            return missingRanges;
+        }
+        if(lower < nums[0])
+            missingRanges.add(Arrays.asList(lower, nums[0]-1));
+        for(int i=0;i<n-1;i++){
+            if(nums[i]+1==nums[i+1]) continue;
+            int l_bound = nums[i]+1;
+            int u_bound = nums[i+1]-1;
+            missingRanges.add(List.of(l_bound, u_bound));
         }
         if(nums[n-1]<upper){
             missingRanges.add(List.of(nums[n-1]+1, upper));
