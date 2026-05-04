44fffc58c2572fb1d2772127b4a5e34c52ef4321
subh <subhranshu497@gmail.com>
2026-05-03 21:32:12 -0700

    05042026

---
+++ com/leetcode/may/RotateImage.java
@@ -0,0 +1,34 @@
+package com.com.leetcode.may;
+
+public class RotateImage {
+    public static void main(String[] args) {
+        int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotateII(matrix);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void rotateII(int[][] matrix) {
+        int n = matrix.length;
+        // transpose
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // reverse each row
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
+}
