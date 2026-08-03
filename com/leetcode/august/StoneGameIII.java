82daaf60fcbe1c1b58c01962c659f4b2e019b7a4
subh <subhranshu497@gmail.com>
2026-08-02 22:23:32 -0500

    08032026

---
+++ com/leetcode/august/StoneGameIII.java
@@ -0,0 +1,45 @@
+package com.com.leetcode.august;
+
+import java.util.Arrays;
+
+public class StoneGameIII {
+    private static int [] t;
+    public static void main(String[] args) {
+        int [] stoneValue ={1,2,3,7};
+        String res = stoneGameIII(stoneValue);
+        System.out.println(res);
+    }
+
+    private static String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        t = new int[n+1];
+        Arrays.fill(t,-1);
+        if(n==1){
+            if(stoneValue[0]<0) return "Bob";
+            else if (stoneValue[0]>0) return "Alice";
+            else return "Tie";
+        }
+        int val = stoneGameIIIRecursion(stoneValue, n,0);
+        if(val==0) return "Tie";
+        else if(val>0) return "Alice";
+        else return "Bob";
+    }
+
+    private static int stoneGameIIIRecursion(int[] stoneValue, int n, int i) {
+
+        if(i>=n)return 0;
+        if(t[i] !=-1)return t[i];
+        int firstmove = stoneValue[i]-stoneGameIIIRecursion(stoneValue,n,i+1);
+        int max= firstmove;
+        max = Math.max(max, firstmove);
+        if(i+1 <n){
+            int secondMove = stoneValue[i]+stoneValue[i+1]-stoneGameIIIRecursion(stoneValue,n,i+2);
+            max = Math.max(max, secondMove);
+        }
+        if(i+2 < n){
+            int thirdMove = stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-stoneGameIIIRecursion(stoneValue,n,i+3);
+            max = Math.max(max, thirdMove);
+        }
+        return t[i]=max;
+    }
+}
