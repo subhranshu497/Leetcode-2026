103c069aecff19773e71495e68ef8c40098ecc24
subh <subhranshu497@gmail.com>
2026-08-19 19:54:03 -0700

    08192026

--- com/leetcode/august/CinemaSeatAllocation.java
+++ com/leetcode/august/CinemaSeatAllocation.java
@@ -1,49 +1,69 @@
 package com.leetcode.august;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class CinemaSeatAllocation {
     public static void main(String[] args) {
-        int n = 3;
-        int [][] reservedSeats = {{1,2},{1,3},{1,8},{2,6},{3,1},{3,10}};
+        int n = 2;
+        int [][] reservedSeats = {{2,1},{1,8},{2,6}};
         int max = maxNumberOfFamilies(reservedSeats,n);
         System.out.println(max);
     }
 
     private static int maxNumberOfFamilies(int[][] reservedSeats, int n) {
-        Map<Integer, Integer> rowBitmasks = new HashMap<>();
-
-        // Build a 10-bit mask for rows with reserved seats
-        for (int[] seat : reservedSeats) {
-            int row = seat[0];
-            int col = seat[1];
-            rowBitmasks.put(row, rowBitmasks.getOrDefault(row, 0) | (1 << col));
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int[] rs:reservedSeats){
+            int row = rs[0];
+            int seat = rs[1];
+            int mask = 1<<seat;
+            map.put(row, mask);
         }
+        int res = n-map.size();
+        int groupAMask = (1<<2) | (1<<3) | (1<<4) | (1<<5);
+        int groupBMask = (1<<4) | (1<<5) | (1<<6) | (1<<7);
+        int groupCMask = (1<<6) | (1<<7) | (1<<8) | (1<<9);
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int bookedSeatsMask = entry.getValue();
 
-        // Unreserved rows can fit 2 families each
-        int res = (n - rowBitmasks.size()) * 2;
+            boolean groupA = (bookedSeatsMask & groupAMask) == 0;
+            boolean groupB = (bookedSeatsMask & groupAMask) == 0;
+            boolean groupC = (bookedSeatsMask & groupAMask) == 0;
 
-        // Check seat availability using bitwise AND operations
-        // 0b0000111100 = seats 2,3,4,5 (left)
-        // 0b0011110000 = seats 4,5,6,7 (middle)
-        // 0b1111000000 = seats 6,7,8,9 (right)
-        int leftMask = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
-        int middleMask = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
-        int rightMask = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
-
-        for (int mask : rowBitmasks.values()) {
-            boolean canLeft = (mask & leftMask) == 0;
-            boolean canRight = (mask & rightMask) == 0;
-            boolean canMiddle = (mask & middleMask) == 0;
-
-            if (canLeft && canRight) {
-                res += 2; // Can fit both left and right families
-            } else if (canLeft || canRight || canMiddle) {
-                res += 1; // Can fit at least one family
-            }
+            if(groupA && groupC)
+                res += 2;
+            else if(groupA || groupB || groupC)
+                res += 1;
         }
 
         return res;
     }
+
+//    private static int maxNumberOfFamilies(int[][] reservedSeats, int n) {
+//        Map<Integer, Set<Integer>> seats = new HashMap<>();
+//        int availableSeats = 0;
+//        for(int [] reservedSeat:reservedSeats){
+//            seats.computeIfAbsent(reservedSeat[0], k->new HashSet<>()).add(reservedSeat[1]);
+//        }
+//        int emptyRows = n-seats.size();
+//        availableSeats = emptyRows*2;
+//        //group A = 2,3,4,5
+//        //group B = 4,5,6,7
+//        //group C = 6,7,8,9
+//        for(Map.Entry<Integer, Set<Integer>> e:seats.entrySet()){
+//            int row= e.getKey();
+//            Set<Integer> set = e.getValue();
+//            boolean groupA = !set.contains(2) && !set.contains(3) && !set.contains(4) && !set.contains(5);
+//            boolean groupB = !set.contains(4) && !set.contains(5) && !set.contains(6) && !set.contains(7);
+//            boolean groupC = !set.contains(6) && !set.contains(7) && !set.contains(8) && !set.contains(9);
+//            if(groupA && groupC) availableSeats +=2;
+//            else{
+//                if(groupA || groupB || groupC) availableSeats +=1;
+//            }
+//        }
+//
+//        return availableSeats;
+//    }
 }
