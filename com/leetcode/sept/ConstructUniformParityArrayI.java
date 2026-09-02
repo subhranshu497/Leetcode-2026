6cf8ff6a5ae8b41d7609023eac4092d5f636bf31
subh <subhranshu497@gmail.com>
2026-09-01 23:49:16 -0500

    09022026

---
+++ com/leetcode/sept/ConstructUniformParityArrayI.java
@@ -0,0 +1,13 @@
+package com.com.leetcode.sept;
+
+public class ConstructUniformParityArrayI {
+    public static void main(String[] args) {
+        int [] nums1 = {2,3};
+        boolean flag = uniformArray(nums1);
+        System.out.println(flag);
+    }
+
+    private static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
